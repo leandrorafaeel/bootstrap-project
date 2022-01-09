daff1d2221a3c3378499dae31ad4866a391ee466
Leandro Rafael <leandrorafael.dev@gmail.com>
2022-01-08 22:29:53 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+<div align ="center">
+
+# Bootstrap Project
+
+</div>
+
+Projeto desenvolvido com o objetivo de colocar em prática meus conhecimentos em Bootstrap 5. Aqui eu utilizei algumas bibliotecas em Javascript para incrementar a aparência do projeto, como as bibliotecas Isotope JS, Owl Carousel e Progress Bar. Para deixar o projeto responsivo eu utilizei Media Queries.
+
+* Demo: [Clique aqui para ver esse projeto](https://leandrorafaeel.github.io/bootstrap-project/)
+
+## Desktop:
+<div align="left">
+    <img src="assets/img/prints/desktop.png" width="650px">
+</div>
+
+## Mobile:
+<div align="left">
+    <img src="assets/img/prints/mobile.png" width="320px">
+</div>
+
+## Tecnologias:
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+* HTML5
+* CSS3
+* JAVASCRIPT (JQUERY)
+* SASS
+
+# Dependências:
+Ferramentas de dependências que utilizei nesse projeto:
+
+* [Bootstrap 5](https://getbootstrap.com/)
+* [SASS](https://sass-lang.com/)
+* [JQUERY](https://releases.jquery.com/jquery/)
+* [Owl Carousel](https://owlcarousel2.github.io/OwlCarousel2/index.html)
+* [Isotope JS](https://isotope.metafizzy.co/)
+* [Progress Bar](https://kimmobrunfeldt.github.io/progressbar.js/)
+
+## Como usar:
+Clone o repositório abaixo
+
+```
+git clone https://github.com/leandrorafaeel/bootstrap-project
+```
+
+## Autor: 
+<img  border-radius="50%" src="https://avatars.githubusercontent.com/u/86384840?v=4" width="100px" alt="Leandro-Rafael-Imagem"/>
+
+#### Leandro Rafael
+
+[![Linkedin Badge](https://img.shields.io/badge/-LinkedIn-2867B2?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/leandrorafael-dev/)](https://www.linkedin.com/in/leandrorafael-dev/) [![Twitter Badge](https://img.shields.io/badge/-Twitter-1DA1F2?style=flat-square&Color=1DA1F2&logo=twitter&logoColor=white&link=https://twitter.com/leandrorafaelBR)](https://twitter.com/leandrorafaelBR) 
+
+## Licença:
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Este projeto está sob licença do MIT. Veja a licença para mais informações:
+
+[Veja o Copyright](https://github.com/leandrorafaeel/bootstrap-project/blob/master/LICENSE)
---
+++ assets/js/main.js
@@ -0,0 +1,172 @@
+let btnSubir = document.querySelector("#btn-subir");
+
+window.onscroll = function(){
+    if(pageYOffset > 250){
+        btnSubir.style.display = "block";
+    }else{
+        btnSubir.style.display = "none"
+    }
+}
+
+
+// Progress bar
+
+//circle a
+let containerA = document.getElementById("circleA");
+
+let circleA = new ProgressBar.Circle(containerA, {
+
+    color: '#fff',
+    strokeWidth: 8,
+    duration: 1400,
+    from: { color: '#aaa'},
+    to: { color: '#fc5185'},
+
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+
+    
+        var value = Math.round(circle.value() * 500);
+        circle.setText("+" + value);
+    }
+});
+    
+//circle b
+let containerB = document.getElementById("circleB");
+let circleB = new ProgressBar.Circle(containerB, {
+
+    color: '#fff',
+    strokeWidth: 8,
+    duration: 1400,
+    from: { color: '#aaa'},
+    to: { color: '#fc5185'},
+
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+
+    var value = Math.round(circle.value() * 125);
+        circle.setText(value);
+    }
+});
+
+//circle c
+let containerC = document.getElementById("circleC");
+let circleC = new ProgressBar.Circle(containerC, {
+
+    color: '#fff',
+    strokeWidth: 8,
+    duration: 1400,
+    from: { color: '#aaa'},
+    to: { color: '#fc5185'},
+
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+
+    var value = Math.round(circle.value() * 26);
+        circle.setText(value);
+    }
+});
+
+//circle d
+let containerD = document.getElementById("circleD");
+let circleD = new ProgressBar.Circle(containerD, {
+
+    color: '#fff',
+    strokeWidth: 8,
+    duration: 1400,
+    from: { color: '#aaa'},
+    to: { color: '#fc5185'},
+
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+
+    var value = Math.round(circle.value() * 1000);
+        circle.setText("+" + value);
+    }
+});
+// iniciando loaders quando a usuário chegar no elemento
+let dataAreaOffset = $('#dados').offset();
+// stop serve para a animação não carregar mais que uma vez
+let stop = 0;
+
+$(window).scroll(function (e) {
+
+  let scroll = $(window).scrollTop();
+
+  if(scroll > (dataAreaOffset.top - 500) && stop == 0) {
+    circleA.animate(1.0);
+    circleB.animate(1.0);
+    circleC.animate(1.0);
+    circleD.animate(1.0);
+
+    stop = 1;
+  }
+
+});
+    
+
+
+//owl carousel
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        margin: 20,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+        }
+    })
+});
+
+//projetos ativos
+let btnAll = document.querySelector("#btn-all");
+let btnWeb = document.querySelector("#btn-web");
+let btnMobile = document.querySelector("#btn-mobile");
+let btnDesign = document.querySelector("#btn-design");
+
+btnAll.addEventListener("click", function(){
+    checkButton(btnWeb, btnMobile, btnDesign, this);
+})
+
+btnWeb.addEventListener("click", function(){
+    checkButton(btnAll, btnMobile, btnDesign, this);
+})
+
+btnMobile.addEventListener("click", function(){
+    checkButton(btnAll, btnWeb, btnDesign, this);
+})
+
+btnDesign.addEventListener("click", function(){
+    checkButton(btnAll, btnWeb, btnMobile, this);
+})
+
+function checkButton(btn, btn2, btn3, check){
+    btn.classList.remove("active");
+    btn2.classList.remove("active");
+    btn3.classList.remove("active");
+
+    check.classList.add('active');
+}
+
+//isotope
+$('.items').isotope({
+    itemSelector: '.item',
+    layoutMode: 'fitRows'
+});
+
+$('.botoes button').click(function(){
+
+    var selector = $(this).attr('data-filter');
+
+    $('.items').isotope({
+        filter: selector
+    });
+})
